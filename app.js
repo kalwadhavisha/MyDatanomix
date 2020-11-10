f4fc2be9b25fd53495bde2caf19f8f608557fe87
Havisha <havisha.kalwad@gmail.com>
2020-11-09 17:46:29 -0800

    hyperlinks on viz page working

--- app.js
+++ app.js
@@ -194,8 +194,8 @@ mongoose
 		useUnifiedTopology: true,
 	})
 	.then((resultDB) =>
-		app.listen(8080, function () {
-			console.log("App listening on port 8080!");
+		app.listen(5000, function () {
+			console.log("App listening on port 5000!");
 		})
 	)
 	.catch((err) => console.log(err));
