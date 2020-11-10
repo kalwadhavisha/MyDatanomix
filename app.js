94dba45c31dc9454552244b54654c343a07ede08
Havisha <havisha.kalwad@gmail.com>
2020-11-09 17:35:30 -0800

    changed port

--- app.js
+++ app.js
@@ -194,8 +194,8 @@ mongoose
 		useUnifiedTopology: true,
 	})
 	.then((resultDB) =>
-		app.listen(80, function () {
-			console.log("App listening on port 80!");
+		app.listen(8080, function () {
+			console.log("App listening on port 8080!");
 		})
 	)
 	.catch((err) => console.log(err));
